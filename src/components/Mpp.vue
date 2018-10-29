1428c9f9ed94605657ab15a4d5753ead36a1f761
Keller Me <ppmiao0628@outlook.com>
2018-10-29 20:55:21 +0800

    state访问状态对象

--- src/components/Mpp.vue
+++ src/components/Mpp.vue
@@ -4,6 +4,8 @@
     <h1>username:{{ $route.params.username }}</h1>
     <h1>age:{{ $route.params.age }}</h1>
     <h2>{{$store.state.count}}:from state</h2>
+    <!-- <h2>{{count1}}:from state</h2> -->
+    <h2>{{count}}:from state</h2>
     <p>
       <button @click="$store.commit('add')">+</button>
       <button @click="$store.commit('reduce')">-</button>
@@ -13,20 +15,33 @@
 </template>
 
 <script>
-import store from "@/vuex/store"
+import store from "@/vuex/store";
+import { mapState } from "vuex";
 export default {
-  name: 'Mpp',
-  data () {
+  name: "Mpp",
+  data() {
     return {
-      msg: 'Welcome to Your Mpp page'
-    }
+      msg: "Welcome to Your Mpp page"
+    };
   },
-  store
-}
+  store,
+  // computed: {
+  //   count1() {
+  //     return store.state.count + "-computed";
+  //   },
+  // }
+  // computed: mapState({
+  //   count2: function(state) {
+  //     return state.count;
+  //   }
+  // })
+  computed:mapState(['count'])
+};
 </script>
 
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 </style>
