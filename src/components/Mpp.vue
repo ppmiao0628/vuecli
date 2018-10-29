2eaf3cc6569de064c3a87d05de590a1d1616993f
Keller Me <ppmiao0628@outlook.com>
2018-10-29 20:47:10 +0800

    vuex demo

--- src/components/Mpp.vue
+++ src/components/Mpp.vue
@@ -3,18 +3,25 @@
     <h1>{{ msg }}</h1>
     <h1>username:{{ $route.params.username }}</h1>
     <h1>age:{{ $route.params.age }}</h1>
+    <h2>{{$store.state.count}}:from state</h2>
+    <p>
+      <button @click="$store.commit('add')">+</button>
+      <button @click="$store.commit('reduce')">-</button>
+    </p>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import store from "@/vuex/store"
 export default {
   name: 'Mpp',
   data () {
     return {
       msg: 'Welcome to Your Mpp page'
     }
-  }
+  },
+  store
 }
 </script>
 
---
+++ src/vuex/store.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = {
+  count: 1
+}
+const mutations = {
+  add(state) {
+    return state.count++;
+  },
+  reduce(state) {
+    return state.count--;
+  }
+}
+
+export default new Vuex.Store({
+  state, mutations
+})
